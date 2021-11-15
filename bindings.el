2433fcf3b423469a1f8bbb8c077541007d49bc45
ohmyhub <k.warden.89@gmail.com>
2021-11-15 02:06:15 -0800

    keybind for open-config-file, remove kill-frame

--- bindings.el
+++ bindings.el
@@ -4,7 +4,6 @@
       (:prefix ("k" . "Custom")
        :desc "Switch Frame" :n "f" #'select-frame-by-name
        :desc "Insert today's date" :n "t" #'today
-       :desc "Kill frame" :n "k" #'delete-frame
        :desc "Paste from clipboard" :n "p" #'clipboard-yank
        :desc "Paste as a subtree heading" :n "o" #'org-paste-subtree
        :desc "File from Deft dir" :n "d" #'deft-find-file
@@ -12,6 +11,7 @@
        :desc "Export pdf" :n "e" #'org-latex-export-to-pdf
        :desc "Ace window" :n "w" #'ace-window
        :desc "org-capture frame" :n "c" #'+org-capture/open-frame
-       :desc "New empty ORG buffer" "g" #'evil-buffer-org-new
-       :desc "Add directory as project" "a" #'doom/add-directory-as-project
-       :desc "Evaluate buffer and reload fonts" "v" #'evig))
+       :desc "New empty ORG buffer" :n "g" #'evil-buffer-org-new
+       :desc "Add directory as project" :n "a" #'doom/add-directory-as-project
+       :desc "Evaluate buffer and reload fonts" :n "v" #'evig
+       :desc "Open config.org" :n "k" #'org-open-config-file))
