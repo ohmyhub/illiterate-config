58beede126eebbcaabc9300a50903c0c16cd3cb3
ohmyhub <k.warden.89@gmail.com>
2021-11-20 02:52:14 -0800

    Reorganize custom keybinds

--- bindings.el
+++ bindings.el
@@ -1,18 +1,14 @@
 ;;; ~/.doom.d/bindings.el -*- lexical-binding: t; -*-
 
 (map! :leader
-      (:prefix ("k" . "Grab bag")
-       :desc "Switch Frame" :n                          "f"      #'select-frame-by-name
-       :desc "Insert today's date" :n                   "t"      #'today
-       :desc "Paste from clipboard" :n                  "p"      #'clipboard-yank
-       :desc "Paste as a subtree heading" :n            "o"      #'org-paste-subtree
-       :desc "File from Deft dir" :n                    "d"      #'deft-find-file
-       :desc "Select line" :n                           "l"      #'select-line
+      :desc "Open config.org" :n                        "f k"   #'org-open-config-file
+      :desc "Ace window" :n                             "w a"   #'ace-window
+      (:prefix ("k" . "Grab Bag")
        :desc "Export pdf" :n                            "e"      #'org-latex-export-to-pdf
-       :desc "Ace window" :n                            "w"      #'ace-window
-       :desc "org-capture frame" :n                     "c"      #'+org-capture/open-frame
+       :desc "File from Deft dir" :n                    "d"      #'deft-find-file
        :desc "New empty ORG buffer" :n                  "g"      #'evil-buffer-org-new
-       :desc "Add directory as project" :n              "a"      #'doom/add-directory-as-project
-       :desc "Evaluate buffer and reload fonts" :n      "v"      #'evig
-       :desc "Open config.org" :n                       "k"      #'org-open-config-file
+       :desc "Paste as a subtree heading" :n            "o"      #'org-paste-subtree
+       :desc "Paste from clipboard" :n                  "p"      #'clipboard-yank
        :desc "Ranger in emacs" :n                       "r"      #'ranger))
+       :desc "Insert today's date" :n                   "t"      #'today
+       :desc "Evaluate buffer and reload fonts" :n      "v"      #'evig
