e64d4fae948c067acbc15d5b284f91dfe008f8b0
ohmyhub <k.warden.89@gmail.com>
2021-11-24 15:32:51 -0800

    Move the parens down, dejavu

--- bindings.el
+++ bindings.el
@@ -9,6 +9,6 @@
        :desc "New empty ORG buffer" :n                  "g"      #'evil-buffer-org-new
        :desc "Paste as a subtree heading" :n            "o"      #'org-paste-subtree
        :desc "Paste from clipboard" :n                  "p"      #'clipboard-yank
-       :desc "Ranger in emacs" :n                       "r"      #'ranger
+       :desc "Ranger in emacs" :n                       "r"      #'ranger))
        :desc "Insert today's date" :n                   "t"      #'today
-       :desc "Evaluate buffer and reload fonts" :n      "v"      #'evig))
+       :desc "Evaluate buffer and reload fonts" :n      "v"      #'evig
